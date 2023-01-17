93eef32b3e6614924de7d1351ecc8641ac41fbb1
simplyDonald <donaldabuah@gmail.com>
2023-01-16 20:04:08 -0500

    setup header component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,14 +1,29 @@
 import { FC } from 'react';
+import Image from 'next/image';
 
 interface IHeaderProps {
-  
+
 }
 
 const Header: FC<IHeaderProps> = (props) => {
   return (
-    <div>
-      <h1>Hey guys</h1>
-    </div>
+    <header className=''>
+      {/* top nav */}
+      <div>
+        <div> 
+          <Image src='https://links.papareact.com/f90' 
+            className='cursor-pointer'
+            alt="logo"
+            width={150} 
+            height={40} 
+            style={{ objectFit: 'contain' }}
+          />
+        </div>
+      </div>
+
+      {/* bottom nav */}
+      <div></div>
+    </header>
   ) ;
 };
 

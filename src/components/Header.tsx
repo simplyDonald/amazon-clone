ccf683ab8d47c7007bdb804a6d6f722aabceab21
simplyDonald <donaldabuah@gmail.com>
2023-01-18 21:47:22 -0500

    add bottom header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -60,7 +60,21 @@ const Header: FC<IHeaderProps> = (props) => {
       </div>
 
       {/* bottom nav */}
-      <div></div>
+      <div className='flex items-center bg-amazon_blue-light text-white text-sm space-x-3 p-2 pl-6'>
+        <p className='link flex items-center'>
+          <Bars3Icon className="h-6 mr-1" />
+          All
+        </p>
+        <p className='link'>Prime Video</p>
+        <p className='link'>Amazon Business</p>
+        <p className='link'>Today's Deals</p>
+        <p className='hidden lg:inline-flex link'>Electronics</p>
+        <p className='hidden lg:inline-flex link'>Food & Grocery</p>
+        <p className='hidden lg:inline-flex link'>Prime</p>
+        <p className='hidden lg:inline-flex link'>Buy Again</p>
+        <p className='hidden lg:inline-flex link'>Shopper Toolkit</p>
+        <p className='hidden lg:inline-flex link'>Health & Personal Care</p>
+      </div>
     </header>
   );
 };

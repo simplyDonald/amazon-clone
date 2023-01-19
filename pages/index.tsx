b05b4bea3fe247df5a770613eeb72961b307ec76
simplyDonald <donaldabuah@gmail.com>
2023-01-18 22:11:41 -0500

    setup banner component

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,7 @@ import type { NextPage } from "next";
 import Head from "next/head";
 import Image from "next/image";
 import { useSelector, useDispatch } from "react-redux";
+import Banner from "../src/components/Banner";
 import Header from "../src/components/Header";
 import { increment, decrement } from "../src/slices/basketSlice";
 import { itemsInBasket } from "../src/slices/basketSlice";
@@ -17,8 +18,15 @@ const Home: NextPage = () => {
         <title>Amazon-clone</title>
         <link rel="icon" href="/amazon.ico" />
       </Head>
-      
+
       <Header />
+
+      <main className="max-w-screen-2xl mx-auto">
+        {/* Banner */}
+        <Banner />
+
+        {/* Product feed */}
+      </main>
 
     </div>
   );
---
+++ src/components/Banner.tsx
@@ -0,0 +1,14 @@
+import { FC } from 'react';
+
+interface IBannerProps {
+}
+
+const Banner: FC<IBannerProps> = (props) => {
+  return (
+    <div>
+      <h1>Banner</h1>
+    </div>
+  ) ;
+};
+
+export default Banner;

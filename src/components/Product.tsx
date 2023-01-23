671918180ba89bcef2aa0c6240e446d337658190
simplyDonald <donaldabuah@gmail.com>
2023-01-23 01:24:18 -0500

    implement animated product cards

--- src/components/Product.tsx
+++ src/components/Product.tsx
@@ -3,6 +3,7 @@ import React from "react";
 import {FC, useState} from 'react';
 import {IProductProps} from '../typings';
 import {StarIcon} from '@heroicons/react/24/solid';
+import { motion } from "framer-motion";
 
 
 const MAX_RATING = 5;
@@ -22,13 +23,33 @@ const Product: FC<IProductProps> = ({
 
   const [hasPrime] = useState(Math.random() < 0.5);
 
-
+  const itemVariants = {
+    hidden: {
+      opacity: 0,
+      y: 20,
+    },
+    visible: {
+      opacity: 1,
+      y: 0,
+      transition: {
+        type: "tween",
+        duration: 1,
+        delay: 0.3,
+      },
+    },
+  };
 
   return (
-    <div className="relative grid m-5 bg-white z-30 p-10 group">
+    <motion.div
+      className="relative grid m-5 bg-white z-30 p-10 group"
+      variants={itemVariants}
+      initial="hidden"
+      animate="visible"
+      viewport={{ once: true }}
+    >
       <p className="absolute top-2 right-2 text-xs italic">{category}</p>
       <Image
-        className='group-hover:scale-110 transition duration-300 ease-in-out transform justify-self-center'
+        className="group-hover:scale-110 transition duration-300 ease-in-out transform justify-self-center"
         alt=""
         src={image}
         height={200}
@@ -46,18 +67,18 @@ const Product: FC<IProductProps> = ({
           ))}
       </div>
       <p className="text-xs my-2 line-clamp-2">{description}</p>
-      <div className='mb-5'>
+      <div className="mb-5">
         <p>CAD {price}</p>
       </div>
       {hasPrime && (
-        <div className='flex items-center spaxe-x-2 -mt-5'>
-          <img className='w-12' src="https://links.papareact.com/fdw" alt="" />
-          <p className='text-xs text-gray-500'>FREE Next-day Delivery</p>
+        <div className="flex items-center spaxe-x-2 -mt-5">
+          <img className="w-12" src="https://links.papareact.com/fdw" alt="" />
+          <p className="text-xs text-gray-500">FREE Next-day Delivery</p>
         </div>
       )}
 
-      <button className='mt-auto button'>Add to Basket</button>
-    </div>
+      <button className="mt-auto button">Add to Basket</button>
+    </motion.div>
   );
 };
 

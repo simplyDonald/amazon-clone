b373f2dc6df10dedc1a025aba0982f2d15dc7f98
simplyDonald <donaldabuah@gmail.com>
2023-01-22 23:37:21 -0500

    remove basket text

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -36,7 +36,7 @@ const Header: FC<IHeaderProps> = (props) => {
         </div>
 
         {/* right */}
-        <div className="text-white flex items-center text-xs space-x-6 mx-6 whitespace-nowrap">
+        <div className="text-white flex items-center text-xs space-x-6 mx-6 whitespace-nowrap  ">
           <div className="link">
             <p>Hello Jeff Bezos</p>
             <p className=" font-extrabold md:text-sm">Account & Lists</p>
@@ -47,14 +47,11 @@ const Header: FC<IHeaderProps> = (props) => {
             <p className="font-extrabold md:text-sm">& Orders</p>
           </div>
 
-          <div className=" hover:animate-wiggle relative flex items-center link">
-            <span className="absolute top-0 right-0 md:right-7  h-4 w-4 bg-yellow-400 text-center text-black rounded-full">
+          <div className="hover:animate-wiggle relative flex items-center link">
+            <span className="absolute top-0 right-0 h-4 w-4 bg-yellow-400 text-center text-black rounded-full  group-hover:animate-wiggle">
               0
             </span>
-            <ShoppingCartIcon className="h-10 " />
-            <p className="hidden md:inline font-extrabold mt-2 md:text-sm">
-              Basket
-            </p>
+            <ShoppingCartIcon className="h-10" />
           </div>
         </div>
       </div>

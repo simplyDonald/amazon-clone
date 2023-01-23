adff500ef46f7784f60c724e7d7bca95df05732c
simplyDonald <donaldabuah@gmail.com>
2023-01-22 22:48:10 -0500

    add responsive grid

--- src/components/ProductFeed.tsx
+++ src/components/ProductFeed.tsx
@@ -8,8 +8,7 @@ import Product from '../components/Product';
 
 const ProductFeed: FC<{products: Product[]}> = ({products}) => {
   return(
-    <div>
-      <h3>products hashsah</h3>
+    <div className='grid grid-flow-row-dense md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 md:-mt-52 mx-auto'>
       {
         products.map(({id, title, price, description, category, image}) => (
           <Product 

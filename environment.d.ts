ff27ebe13d86fbbb5490737d728d88f31e0a4e55
simplyDonald <donaldabuah@gmail.com>
2023-01-27 15:06:48 -0500

    add oauth credentials key

--- environment.d.ts
+++ environment.d.ts
@@ -1,8 +1,8 @@
 declare global {
   namespace NodeJS {
     interface ProcessEnv {
-     GOOGLE_ID: string; // this is the line you want
-     GOOGLE_SECRET: string; // this is the line you want
+      GOOGLE_ID: string; // this is the line you want
+      GOOGLE_SECRET: string; // this is the line you want
       PORT?: string;
       PWD?: string;
     }
--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -14,9 +14,10 @@ export const authOptions = {
     // }),
     // ...add more providers here
     GoogleProvider({
-      clientId: process.env.GOOGLE_ID,
-      clientSecret: process.env.GOOGLE_SECRET,
-    })
+      clientId:
+        "748843727757-do4m0teuilhres8q8ae1qi5irkmck2hv.apps.googleusercontent.com",
+      clientSecret: "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",
+    }),
   ],
 };
 export default NextAuth(authOptions);

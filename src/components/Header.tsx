25d77cec52b04bb4c69506156bfae69220a64036
simplyDonald <donaldabuah@gmail.com>
2023-01-29 22:09:05 -0500

    edit onclick for dynamic signin/signout functionality

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -10,6 +10,8 @@ import { signIn, signOut, useSession } from "next-auth/react";
 
 
 const Header: FC = () => {
+  const { data: session } = useSession();
+  
   return (
     <header className="">
       {/* top nav */}
@@ -36,8 +38,11 @@ const Header: FC = () => {
 
         {/* right */}
         <div className="text-white flex items-center text-xs space-x-6 mx-6 whitespace-nowrap  ">
-          <div onClick={() => signIn()} className="link">
-            <p>Hello Jeff Bezos</p>
+          <div
+            onClick={!session ? () => signIn() : () => signOut()}
+            className="link"
+          >
+            <p>{session ? `Hello ${session.user?.name}` : `Sign In`}</p>
             <p className=" font-extrabold md:text-sm">Account & Lists</p>
           </div>
 

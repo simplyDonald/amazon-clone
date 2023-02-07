f7e2aa1808363a7470119dfeefd25ac7af5159b4
simplyDonald <donaldabuah@gmail.com>
2023-02-07 17:55:53 -0500

    setup dynamic count and price summation of cart Items

--- pages/checkout.tsx
+++ pages/checkout.tsx
@@ -3,10 +3,9 @@ import Image from 'next/image';
 import React from "react";
 import Header from '../src/components/Header';
 import { useSelector } from 'react-redux';
-import { selectItems } from '../src/slices/basketSlice';
-import { selectCartItems } from '../src/slices/basketSlice';
+import { selectCartItems, selectItems } from '../src/slices/basketSlice';
 import CheckoutProduct from '../src/components/CheckoutProduct';
-import { IProduct, ICheckoutProductProps } from '../src/types/typings';
+import {  ICheckoutProductProps } from '../src/types/typings';
 
 
 interface ICheckoutProps {
@@ -14,7 +13,8 @@ interface ICheckoutProps {
 
 const Checkout: FC<ICheckoutProps> = (props) => {
 
-  const items = useSelector(selectCartItems) as ICheckoutProductProps[] | [];;
+  const cartItems = useSelector(selectCartItems) as ICheckoutProductProps[];
+
 
 
   return (
@@ -32,30 +32,51 @@ const Checkout: FC<ICheckoutProps> = (props) => {
           />
           <div className="flex flex-col p-5 space-y-10 bg-white">
             <h1 className="text-3xl border-b pb-4">
-              {items.length === 0
+              {cartItems.length === 0
                 ? "Your Amazon Basket is empty"
                 : "Shopping Basket"}
             </h1>
 
-            {items.length > 0 && items.map((item) => (
-              <CheckoutProduct
-                key={item.id}
-                id={item.id}
-                rating={item.rating}
-                title={item.title}
-                price={item.price}
-                description={item.description}
-                category={item.category}
-                image={item.image}
-                hasPrime={item.hasPrime}
-                count={item.count}
-              />
-            ))}
+            {cartItems.length > 0 &&
+              cartItems.map((item) => (
+                <CheckoutProduct
+                  key={item.id}
+                  id={item.id}
+                  rating={item.rating}
+                  title={item.title}
+                  price={item.price}
+                  description={item.description}
+                  category={item.category}
+                  image={item.image}
+                  hasPrime={item.hasPrime}
+                  count={item.count}
+                />
+              ))}
           </div>
         </div>
 
         {/* right */}
-        <div></div>
+        <div>
+          {cartItems.length > 0 && (
+            <>
+              <h2>
+                Subtotal (
+                {/* summation of the quantity of items in cartItems */}
+                {cartItems.reduce((count, item) => count + item.count, 0)})
+                items
+              </h2>
+              <span>
+                CAD{" "}
+                {/* mathematical summation of the total price from cartItems */}
+                {cartItems.reduce(
+                  (total, item) =>
+                    Number((total + item.count * item.price).toFixed(2)),
+                  0
+                )}
+              </span>
+            </>
+          )}
+        </div>
       </main>
     </div>
   );
